12a27a2098507f0d06d4164dd07aaafd92ac9fe8
Viraj Thakkar <viraj.dt@asu.edu>
2025-02-05 15:50:24 -0700

    Update cgroup v1 codebase to be similar to v2 codebase

--- cgroup_monitor/v1_manager.py
+++ cgroup_monitor/v1_manager.py
@@ -1,60 +1,185 @@
 import os
 import subprocess
 
+
 class CGroupManager:
-    def __init__(self, cgroup_name, cgroup_base_path="/sys/fs/cgroup", helper_script=""):
+    def __init__(self, cgroup_name, cgroup_base_path="/sys/fs/cgroup", helper_script=None):
+        '''
+        Initialize the CGroupManager object. If helper_script is not provided,
+        the script will use the default method of managing cgroups.
+
+        One CGroupManager object is responsible for managing one cgroup. Hence,
+        the code expects the cgoup_name to be provided while initializing the object.
+
+        Parameters:
+        - cgroup_name (str): Name of the cgroup. Necessary argument.
+        - cgroup_base_path (str): Base path of the cgroup. Default is /sys/fs/cgroup.
+        - helper_script (str): Path to the helper script to manage cgroups. Default is None.
+
+        Returns:
+        - None
+        '''
         self.cgroup_name = cgroup_name
         self.cgroup_base_path = cgroup_base_path
         self.cpu_path = os.path.join(self.cgroup_base_path, "cpu", cgroup_name)
         self.mem_path = os.path.join(self.cgroup_base_path, "memory", cgroup_name)
         self.helper_script = None
 
-        if helper_script != "":
+        if helper_script is not None:
             self.helper_script = os.path.join(os.path.dirname(__file__), helper_script)
 
+    def _run_command(self, runner_cmd, helper_cmd, sudo):
+        '''
+        Internal function to run given command conditionally.
+        If sudo is True, the command will be run with sudo privileges.
+        Otherwise, the command will be run as is.
+        If helper_script is provided, the command will be run using the helper script.
+
+        Parameters:
+        - runner_cmd (str): Command to run using subprocess.run.
+        - helper_cmd (list): Command to run using subprocess.run.
+        - sudo (bool): Whether to use sudo to run the command.
+
+        Returns:
+        - returncode (int): Return whether the command was successful.
+        '''
+        try:
+            if sudo:
+                runner_cmd = f"sudo sh -c '{runner_cmd}'"
+                helper_cmd.insert(0, "sudo")
+
+            if self.helper_script is None:
+                proc = subprocess.run(runner_cmd, shell=True, check=True)
+            else:
+                proc = subprocess.run(helper_cmd, check=True)
+
+            return proc.returncode
+
+        except Exception as e:
+            raise Exception(f"Failed to run command: {e}")
+
     def create_cgroup(self):
+        '''
+        If non-existant, create cgroup and take ownership.
+        Note, this command requires sudo privileges.
+
+        Parameters:
+        - None
+
+        Returns:
+        - returncode (int): Return whether the command was successful.
+        '''
+        # Create cgroup if it does not exist
         for path in [self.cpu_path, self.mem_path]:
             if not os.path.exists(path):
                 if self.helper_script is None:
-                    subprocess.run(["sudo", "mkdir", path], check=True)
+                    proc = subprocess.run(["sudo", "mkdir", path], check=True)
                 else:
-                    subprocess.run(["sudo", self.helper_script, "create", path], check=True)
+                    proc = subprocess.run(["sudo", self.helper_script, "create", path], check=True)
 
-    def set_cpu_limit(self, quota, period=100000):
+                if proc.returncode != 0:
+                    raise Exception(f"Failed to create cgroup: {path}")
+
+        # Take ownership of cgroup
+        for path in [self.cpu_path, self.mem_path]:
+            if self.helper_script is None:
+                proc = subprocess.run(["sudo", "chown", "-R", f"{os.getuid()}:{os.getgid()}", path], check=True)
+            else:
+                proc = subprocess.run(["sudo", self.helper_script, "chown", os.getuid(), path], check=True)
+
+            if proc.returncode != 0:
+                raise Exception(f"Failed to take ownership of cgroup: {path}")
+
+        return proc.returncode
+
+    def set_cpu_limit(self, quota, period=100000, sudo=False):
+        '''
+        Set the CPU limits.
+
+        Parameters:
+        - quota (int): CPU quota in microseconds.
+        - period (int): CPU period in microseconds. Default is 100000.
+        - sudo (bool): Whether to use sudo to run the command. Default is True.
+
+        Returns:
+        - returncode (int): Return whether the command was successful.
+        '''
         cpu_quota_path = os.path.join(self.cpu_path, "cpu.cfs_quota_us")
         cpu_period_path = os.path.join(self.cpu_path, "cpu.cfs_period_us")
 
-        if self.helper_script is None:
-            with open(cpu_quota_path, "w") as f:
-                f.write(str(quota))
-            with open(cpu_period_path, "w") as f:
-                f.write(str(period))
-        else:
-            subprocess.run(["sudo", self.helper_script, "write", cpu_quota_path, str(quota)], check=True)
-            subprocess.run(["sudo", self.helper_script, "write", cpu_period_path, str(period)], check=True)
+        runner_cmd_quota = f"echo \"{quota * period}\" > {cpu_quota_path}"
+        runner_cmd_period = f"echo \"{period}\" > {cpu_period_path}"
+        helper_cmd_quota = [self.helper_script, "write", cpu_quota_path, str(quota)]
+        helper_cmd_period = [self.helper_script, "write", cpu_period_path, str(period)]
 
-    def set_memory_limit(self, limit):
+        self._run_command(runner_cmd_quota, helper_cmd_quota, sudo)
+        return self._run_command(runner_cmd_period, helper_cmd_period, sudo)
+
+    def set_memory_limit(self, limit, sudo=False):
+        '''
+        Set the memory limit in bytes.
+
+        Parameters:
+        - limit (int): Memory limit in bytes.
+        - sudo (bool): Whether to use sudo to run the command. Default is True.
+
+        Returns:
+        - returncode (int): Return whether the command was successful.
+        '''
         mem_limit_path = os.path.join(self.mem_path, "memory.limit_in_bytes")
+        runner_cmd = f"echo \"{limit}\" > {mem_limit_path}"
+        helper_cmd = [self.helper_script, "write", mem_limit_path, str(limit)]
 
-        if self.helper_script is None:
-            with open(mem_limit_path, "w") as f:
-                f.write(str(limit))
-        else:
-            subprocess.run(["sudo", self.helper_script, "write", mem_limit_path, str(limit)], check=True)
+        return self._run_command(runner_cmd, helper_cmd, sudo)
 
-    def add_process(self, pid):
+    def set_memory_swap_limit(self, limit, sudo=False):
+        '''
+        Set the memory+swap limit in bytes.
+        Note: This sets maximum amount for the sum of memory and swap usage.
+
+        Parameters:
+        - limit (int): Memory+swap limit in bytes.
+        - sudo (bool): Whether to use sudo to run the command.
+
+        Returns:
+        - returncode (int): Return whether the command was successful.
+        '''
+        mem_swap_limit_path = os.path.join(self.mem_path, "memory.memsw.limit_in_bytes")
+        runner_cmd = f"echo \"{limit}\" > {mem_swap_limit_path}"
+        helper_cmd = [self.helper_script, "write", mem_swap_limit_path, str(limit)]
+
+        return self._run_command(runner_cmd, helper_cmd, sudo)
+
+    def add_process(self, pid, sudo=False):
+        '''
+        Add a process to the cgroup.
+
+        Parameters:
+        - pid (int): Process ID to add to the cgroup.
+        - sudo (bool): Whether to use sudo to run the command. Default is True.
+
+        Returns:
+        - returncode (int): Return whether the command was successful.
+        '''
         for path in [self.cpu_path, self.mem_path]:
             procs_path = os.path.join(path, "cgroup.procs")
-            if self.helper_script is None:
-                with open(procs_path, "w") as f:
-                    f.write(str(pid))
-            else:
-                subprocess.run(["sudo", self.helper_script, "write", procs_path, str(pid)], check=True)
+            runner_cmd = f"echo \"{pid}\" > {procs_path}"
+            helper_cmd = [self.helper_script, "write", procs_path, str(pid)]
 
-    def delete_cgroup(self):
+            self._run_command(runner_cmd, helper_cmd, sudo)
+
+    def delete_cgroup(self, sudo=False):
+        '''
+        Delete the cgroup directories.
+
+        Parameters:
+        - sudo (bool): Whether to use sudo to run the command. Default is True.
+
+        Returns:
+        - returncode (int): Return whether the command was successful.
+        '''
         for path in [self.cpu_path, self.mem_path]:
             if os.path.exists(path):
-                if self.helper_script is None:
-                    subprocess.run(["sudo", "rmdir", path], check=True)
-                else:
-                    subprocess.run(["sudo", self.helper_script, "delete", path], check=True)
+                runner_cmd = f"rmdir {path}"
+                helper_cmd = [self.helper_script, "delete", path]
+                self._run_command(runner_cmd, helper_cmd, sudo)
--- cgroup_monitor/v1_monitor.py
+++ cgroup_monitor/v1_monitor.py
@@ -5,8 +5,20 @@ import threading
 
 class CGroupMonitor:
     def __init__(self, cgroup_name="", cgroup_base_path="/sys/fs/cgroup"):
+        '''
+        Initialize the CGroupMonitor object.
+
+        Parameters:
+        - cgroup_name (str): Name of the cgroup. Default is an empty string.
+        - cgroup_base_path (str): Base path of the cgroup. Default is /sys/fs/cgroup.
+
+        Returns:
+        - None
+        '''
         self.cgroup_name = cgroup_name
         self.cgroup_base_path = cgroup_base_path
+        self.cpu_path = os.path.join(self.cgroup_base_path, "cpu", cgroup_name)
+        self.mem_path = os.path.join(self.cgroup_base_path, "memory", cgroup_name)
 
         self.monitoring = False
         self.cpu_usage_percentages = []
@@ -14,84 +26,162 @@ class CGroupMonitor:
         self.monitor_thread = None
         self.start_time = None
 
-    def get_cpu_usage_us(self):
-        """
-        In cgroup v1, we read from cpuacct.usage (in nanoseconds).
-        """
-        cpu_stat_path = os.path.join(self.cgroup_base_path, "cpu", self.cgroup_name, "cpuacct.usage")
+    def _read_file(self, path):
+        '''
+        Internal method to read the contents of a file.
+
+        Parameters:
+        - path (str): Path to the file.
+
+        Returns:
+        - content (str): Content of the file.
+        '''
         try:
-            with open(cpu_stat_path, "r") as f:
-                return int(f.read().strip())
+            with open(path, "r") as f:
+                return f.read().strip()
         except FileNotFoundError:
-            return 0
+            return None
+
+    def get_cpu_usage_us(self):
+        '''
+        Get the cumulative CPU usage in microseconds.
+
+        Parameters:
+        - None
+
+        Returns:
+        - usage_usec (int): CPU usage in microseconds.
+        '''
+        cpu_stat_path = os.path.join(self.cpu_path, "cpuacct.usage")
+        content = self._read_file(cpu_stat_path)
+        return int(content) if content else 0
 
     def get_cpu_limit(self):
-        """
-        Return (quota, period).
-        """
-        cpu_quota_path = os.path.join(self.cgroup_base_path, "cpu", self.cgroup_name, "cpu.cfs_quota_us")
-        cpu_period_path = os.path.join(self.cgroup_base_path, "cpu", self.cgroup_name, "cpu.cfs_period_us")
-        quota, period = None, None
-        try:
-            with open(cpu_quota_path, "r") as f_quota:
-                quota = int(f_quota.read().strip())
-        except FileNotFoundError:
-            pass
-        try:
-            with open(cpu_period_path, "r") as f_period:
-                period = int(f_period.read().strip())
-        except FileNotFoundError:
-            pass
+        '''
+        Get the CPU limit in microseconds.
+
+        Parameters:
+        - None
+
+        Returns:
+        - quota (int): CPU quota in microseconds.
+        - period (int): CPU period in microseconds.
+        '''
+        cpu_quota_path = os.path.join(self.cpu_path, "cpu.cfs_quota_us")
+        cpu_period_path = os.path.join(self.cpu_path, "cpu.cfs_period_us")
+
+        quota_content = self._read_file(cpu_quota_path)
+        period_content = self._read_file(cpu_period_path)
+        quota = int(quota_content) if quota_content else None
+        period = int(period_content) if period_content else None
         return quota, period
 
     def get_memory_usage(self):
-        memory_current_path = os.path.join(self.cgroup_base_path, "memory", self.cgroup_name, "memory.usage_in_bytes")
-        try:
-            with open(memory_current_path, "r") as f:
-                return int(f.read().strip())
-        except FileNotFoundError:
-            return 0
+        '''
+        Get the current memory usage in bytes.
+
+        Parameters:
+        - None
+
+        Returns:
+        - memory_usage (int): Memory usage in bytes.
+        '''
+        memory_current_path = os.path.join(self.mem_path, "memory.usage_in_bytes")
+        content = self._read_file(memory_current_path)
+        return int(content) if content else 0
 
     def get_memory_limit(self):
-        memory_max_path = os.path.join(self.cgroup_base_path, "memory", self.cgroup_name, "memory.limit_in_bytes")
-        try:
-            with open(memory_max_path, "r") as f:
-                return int(f.read().strip())
-        except FileNotFoundError:
-            return 0
+        '''
+        Get the memory limit in bytes.
+
+        Parameters:
+        - None
+
+        Returns:
+        - memory_limit (int): Memory limit in bytes.
+        '''
+        memory_max_path = os.path.join(self.mem_path, "memory.limit_in_bytes")
+        content = self._read_file(memory_max_path)
+        return int(content) if content else None
+
+    def get_swap_limit(self):
+        '''
+        Get the memory+swap limit in bytes.
+
+        Parameters:
+        - None
+
+        Returns:
+        - memory_swap_limit (int): Memory+swap limit in bytes.
+        '''
+        memory_swap_max_path = os.path.join(self.mem_path, "memory.memsw.limit_in_bytes")
+        content = self._read_file(memory_swap_max_path)
+        return int(content) if content else None
 
     def _monitor(self, interval):
+        '''
+        Internal method to monitor CPU and memory usage.
+
+        Parameters:
+        - interval (int): Monitoring interval in seconds.
+
+        Returns:
+        - None
+        '''
         prev_cpu_usage = self.get_cpu_usage_us()
         while self.monitoring:
             time.sleep(interval)
+
+            # CPU percentage calculation
             current_cpu_usage = self.get_cpu_usage_us()
             delta = current_cpu_usage - prev_cpu_usage
             prev_cpu_usage = current_cpu_usage
 
             quota, period = self.get_cpu_limit()
-            if quota is None or quota < 0:
-                quota = os.cpu_count() * 100000
-                period = 100000
-            num_cores = quota / period
+            num_cores = quota / period if quota else os.cpu_count()
             total_cpu_time_available = num_cores * (interval * 1e9)
-
             cpu_usage_percent = (delta / total_cpu_time_available) * 100
+
+            # Store results
             self.cpu_usage_percentages.append(cpu_usage_percent)
             self.memory_usage.append(self.get_memory_usage())
 
     def start_monitor(self, interval=1):
+        '''
+        Start monitoring CPU and memory usage.
+
+        Parameters:
+        - interval (int): Monitoring interval in seconds. Default is 1.
+            Uses time.sleep() and can be float value for higher precision.
+
+        Returns:
+        - None
+        '''
         if self.monitoring:
             raise RuntimeError("Monitoring is already running.")
+
         self.monitoring = True
-        self.cpu_usage_percentages.clear()
-        self.memory_usage.clear()
+        self.cpu_usage_percentages = []
+        self.memory_usage = []
         self.start_time = time.time()
         self.monitor_thread = threading.Thread(
             target=self._monitor, args=(interval,), daemon=True
         )
         self.monitor_thread.start()
 
-    def get_last_n_stats(self, n=1):
+    def get_last_n_stats(self, n=1, info_level=0):
+        '''
+        Get the last n stats recorded.
+
+        Parameters:
+        - n (int): Number of stats to retrieve. Default is 1.
+        - info_level (int): Level of information to return. Default is 0.
+            - 0: Return average and max usage stats.
+            - 1: Return detailed stats including all recorded values.
+
+        Returns:
+        - stats (dict): Dictionary containing average and max usage stats.
+        '''
         if not self.monitoring:
             raise RuntimeError("Monitoring is not running.")
 
@@ -99,26 +189,63 @@ class CGroupMonitor:
                    if self.cpu_usage_percentages else 0)
         avg_mem = (sum(self.memory_usage[-n:]) / n
                    if self.memory_usage else 0)
-        avg_mem_gb = avg_mem / (1024 ** 3)
-        avg_mem_pct = (avg_mem / self.get_memory_limit()) * 100 if self.get_memory_limit() else 0
+
+        avg_memory_gb = avg_mem / (1024 ** 3)
+        avg_memory_percent = (avg_mem / self.get_memory_limit()) * 100 if self.get_memory_limit() else 0
 
         max_cpu = max(self.cpu_usage_percentages) if self.cpu_usage_percentages else 0
         max_mem = max(self.memory_usage) if self.memory_usage else 0
-        max_mem_gb = max_mem / (1024 ** 3)
-        max_mem_pct = (max_mem / self.get_memory_limit()) * 100 if self.get_memory_limit() else 0
+        max_memory_gb = max_mem / (1024 ** 3)
+        max_memory_percent = (max_mem / self.get_memory_limit()) * 100 if self.get_memory_limit() else 0
+
+        if info_level == 1:
+            return {
+                "average_cpu_usage_percent": round(avg_cpu, 2),
+                "max_cpu_usage_percent": round(max_cpu, 2),
+                "cpu_usage_percentage_list": self.cpu_usage_percentages[-n:],
+                "average_memory_usage_gib": round(avg_memory_gb, 2),
+                "max_memory_usage_gib": round(max_memory_gb, 2),
+                "average_memory_usage_percent": round(avg_memory_percent, 2),
+                "max_memory_usage_percent": round(max_memory_percent, 2),
+                "memory_usage_bytes_list": self.memory_usage[-n:],
+            }
 
         return {
             "average_cpu_usage_percent": round(avg_cpu, 2),
-            "average_memory_usage_gib": round(avg_mem_gb, 2),
-            "average_memory_usage_percent": round(avg_mem_pct, 2),
             "max_cpu_usage_percent": round(max_cpu, 2),
-            "max_memory_usage_gib": round(max_mem_gb, 2),
-            "max_memory_usage_percent": round(max_mem_pct, 2),
+            "average_memory_usage_gib": round(avg_memory_gb, 2),
+            "max_memory_usage_gib": round(max_memory_gb, 2),
+            "average_memory_usage_percent": round(avg_memory_percent, 2),
+            "max_memory_usage_percent": round(max_memory_percent, 2),
         }
 
-    def stop_monitor(self):
+    def stop_monitor(self, info_level=0):
+        '''
+        Stop monitoring and return average and max usage stats.
+        ```
+        Returns:
+            dict = {
+                "average_cpu_usage_percent": float,
+                "max_cpu_usage_percent": float,
+                "average_memory_usage_gib": float,
+                "max_memory_usage_gib": float,
+                "average_memory_usage_percent": float,
+                "max_memory_usage_percent": float,
+                "monitoring_duration_s": float
+            }
+        ```
+
+        Parameters:
+        - info_level (int): Level of information to return. Default is 0.
+            - 0: Return average and max usage stats.
+            - 1: Return detailed stats including all recorded values.
+
+        Returns:
+        - stats (dict): Dictionary containing average and max usage stats.
+        '''
         if not self.monitoring:
             raise RuntimeError("Monitoring is not running.")
+
         self.monitoring = False
         self.monitor_thread.join()
         total_time = time.time() - self.start_time
@@ -127,21 +254,35 @@ class CGroupMonitor:
                    if self.cpu_usage_percentages else 0)
         avg_mem = (sum(self.memory_usage) / len(self.memory_usage)
                    if self.memory_usage else 0)
-        avg_mem_gb = avg_mem / (1024 ** 3)
+        avg_memory_gb = avg_mem / (1024 ** 3)
         mem_limit = self.get_memory_limit()
-        avg_mem_pct = (avg_mem / mem_limit) * 100 if mem_limit else 0
+        avg_memory_percent = (avg_mem / mem_limit) * 100 if mem_limit else 0
 
         max_cpu = max(self.cpu_usage_percentages) if self.cpu_usage_percentages else 0
         max_mem = max(self.memory_usage) if self.memory_usage else 0
-        max_mem_gb = max_mem / (1024 ** 3)
-        max_mem_pct = (max_mem / mem_limit) * 100 if mem_limit else 0
+        max_memory_gb = max_mem / (1024 ** 3)
+        max_memory_percent = (max_mem / mem_limit) * 100 if mem_limit else 0
+
+        if info_level == 1:
+            return {
+                "average_cpu_usage_percent": round(avg_cpu, 2),
+                "max_cpu_usage_percent": round(max_cpu, 2),
+                "cpu_usage_percentage_list": self.cpu_usage_percentages,
+                "average_memory_usage_gib": round(avg_memory_gb, 2),
+                "max_memory_usage_gib": round(max_memory_gb, 2),
+                "average_memory_usage_percent": round(avg_memory_percent, 2),
+                "max_memory_usage_percent": round(max_memory_percent, 2),
+                "memory_usage_bytes_list": self.memory_usage,
+                "start_time": self.start_time,
+                "monitoring_duration_s": round(total_time, 2),
+            }
 
         return {
             "average_cpu_usage_percent": round(avg_cpu, 2),
-            "average_memory_usage_gib": round(avg_mem_gb, 2),
-            "average_memory_usage_percent": round(avg_mem_pct, 2),
             "max_cpu_usage_percent": round(max_cpu, 2),
-            "max_memory_usage_gib": round(max_mem_gb, 2),
-            "max_memory_usage_percent": round(max_mem_pct, 2),
+            "average_memory_usage_gib": round(avg_memory_gb, 2),
+            "max_memory_usage_gib": round(max_memory_gb, 2),
+            "average_memory_usage_percent": round(avg_memory_percent, 2),
+            "max_memory_usage_percent": round(max_memory_percent, 2),
             "monitoring_duration_s": round(total_time, 2),
         }
